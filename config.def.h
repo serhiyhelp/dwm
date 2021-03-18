4e1bf10ff7ae43e6a7bd0324fdda00e32f4c4203
sergiyhelp <sergiyhelp@gmail.com>
2021-03-18 15:04:53 +0200

    save1

--- config.def.h
+++ config.def.h
@@ -3,16 +3,14 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
 
 static const char col_bg[]       = "#073642";
 static const char col_fg[]       = "#eee8d5";
@@ -30,6 +28,14 @@ static const char *colors[][3]      = {
     [SchemeInfoSel]     = { col_fg, col_bg, col_dd },
 
 	[SchemeStatus]      = { col_fg, col_bg,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+};
+
+static const char *fonts[] =
+{
+    "Fantasque Sans Mono:size=8",
+    "Material Icons:size=10",
+    "FontAwesome:size=10",
+    "Noto Color Emoji:size=10"
 };
 
 static const char *const autostart[] = {
@@ -93,6 +99,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *layoutmenu_cmd = "layoutmenu.sh";
 
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
 static char *statuscmds[] = { "notify-send Mouse$BUTTON", "notify-send hello" };
@@ -100,58 +107,49 @@ static char *statuscmd[] = { "/bin/sh", "-c", NULL, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
-	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
-	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_6,      incrihgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_7,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_8,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,   XK_Return,    spawn,          {.v = termcmd } },
+	{ MODKEY,             XK_d,         spawn,          SHCMD("rofi -show drun")      },
+	{ MODKEY|ShiftMask,   XK_s,         spawn,           SHCMD("flameshot gui")       },
+	{ MODKEY|ShiftMask,   XK_q,         spawn,           SHCMD("manage-power.sh")       },
+
+	{ MODKEY,             XK_b,         togglebar,      {0} },
+	//{ MODKEY,             XK_s,         togglesticky,   {0} },
+	//{ MODKEY,             XK_f,         togglefullscr,  {0} },
+	//{ MODKEY,             XK_apostrophe, toggleAttachBelow,   {0} },
+
+	{ MODKEY,             XK_minus,     incnmaster,     {.i = -1 } },
+	{ MODKEY,             XK_equal,     incnmaster,     {.i = +1 } },
+
+	{ MODKEY,             XK_j,         focusstack,     {.i = +1 } },
+	{ MODKEY,             XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY,             XK_h,         setmfact,       {.f = -0.05} },
+	{ MODKEY,             XK_l,         setmfact,       {.f = +0.05} },
+
+	{ MODKEY|ShiftMask,   XK_h,         setcfact,       {.f = +0.25} },
+	{ MODKEY|ShiftMask,   XK_l,         setcfact,       {.f = -0.25} },
+	{ MODKEY|ShiftMask,   XK_j,         movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,   XK_k,         movestack,      {.i = -1 } },
+
+	{ MODKEY,             XK_t,         setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,             XK_m,         setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,             XK_g,         setlayout,      {.v = &layouts[7]} },
+
+	{ MODKEY,             XK_0,         view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,   XK_0,         tag,            {.ui = ~0 } },
+
+	{ MODKEY,             XK_q,         killclient,     {0} },
+	{ MODKEY,             XK_Return,    zoom,           {0} },
+	{ MODKEY|ShiftMask,   XK_e,         quit,           {0} },
+
+	TAGKEYS(XK_1, 0)
+	TAGKEYS(XK_2, 1)
+	TAGKEYS(XK_3, 2)
+	TAGKEYS(XK_4, 3)
+	TAGKEYS(XK_5, 4)
+	TAGKEYS(XK_6, 5)
+	TAGKEYS(XK_7, 6)
+	TAGKEYS(XK_8, 7)
+	TAGKEYS(XK_9, 8)
 };
 
 /* button definitions */
@@ -159,7 +157,7 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        layoutmenu,     {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button1,        spawn,          {.v = statuscmd } },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = statuscmd } },
--- config.h
+++ config.h
@@ -93,6 +93,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *layoutmenu_cmd = "layoutmenu.sh";
 
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
 static char *statuscmds[] = { "notify-send Mouse$BUTTON", "notify-send hello" };
@@ -159,7 +160,7 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        layoutmenu,     {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button1,        spawn,          {.v = statuscmd } },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = statuscmd } },
